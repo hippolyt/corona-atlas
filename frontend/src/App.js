c4a82b6a648a8eb76022d574baa55706f2f1860c
Sean Eulenberg <sean.eulenberg@rwth-aachen.de>
2020-03-22 15:32:20 +0100

    added first version of terminuebersicht

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import { Navbar, Nav, Col, Container, Row } from 'react-bootstrap'
 import { BookingFlow } from './components/BookingFlow'
+import { AppointmentOverview } from './components/AppointmentOverview'
 
 function App() {
   return (
@@ -19,13 +20,7 @@ function App() {
         </Navbar.Collapse>
       </Navbar>
 
-      <Container>
-        <Row className="mb-4">
-          <Col className="mt-4">
-            <BookingFlow />
-          </Col>
-        </Row>
-      </Container>
+      <AppointmentOverview />
     </>
   );
 }
