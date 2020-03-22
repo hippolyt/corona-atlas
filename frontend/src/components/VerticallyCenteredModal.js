003a17b4957f73afe90fd3ca338517cd2d256eb1
Sean Eulenberg <sean.eulenberg@rwth-aachen.de>
2020-03-22 13:53:57 +0100

    modal component

---
+++ frontend/src/components/VerticallyCenteredModal.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+
+
+export function VerticallyCenteredModal(props) {
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    { props.title }
+          </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <p>
+                    {props.text}
+          </p>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={() => alert("Make me do something")}>{ props.confirmButtonText }</Button>
+                <Button onClick={props.onHide}>Abbrechen</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
