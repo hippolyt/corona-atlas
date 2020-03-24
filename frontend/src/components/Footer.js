953b88b70c77e108af379b01bbd26af41c4a4481
Sean Eulenberg <sean.eulenberg@rwth-aachen.de>
2020-03-24 13:20:24 +0100

    footer update

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,15 +7,18 @@ export function Footer() {
             <div className="container" style={{ minHeight: "15vh" }}>
                 <div className="row" style={{ minHeight: "15vh" }}>
                     <div className="col align-self-center mt-4 mb-4 text-center">
-                        <p className="text-white text-center m-0 font-weight-bold">Corona Testing Information and Planning Plattform
-                        </p>
+                        <p className="text-white text-center m-0 font-weight-bold">Corona Testing Information and Planning Plattform<br />
+                        Check out our project on <a href="https://www.youtube.com/watch?v=UYhM87C_xqA" className="text-success">Youtube</a><br />
+                        Check out our project on <a href="https://devpost.com/software/corona-atlas-de" className="text-success">Devpost</a><br />
+                        Check out our project on <a href="https://github.com/hippolyt/corona-atlas" className="text-success">Github</a></p>
                     </div>
                     <div className="col align-self-center mt-4 mb-4 text-center">
-                        <a href="testcenter"><p className="text-white text-center m-0 font-weight-bold">Patientenübersicht-Demo</p></a>
-                        <a href="booking"><p className="text-white text-center m-0 font-weight-bold">Buchungsprozess-Demo</p></a>
-                        <a href="cases"><p className="text-white text-center m-0 font-weight-bold">Terminübersicht-Demo</p></a>
-                        <a href="managedoctors"><p className="text-white text-center m-0 font-weight-bold">Ärzteverwaltung-Demo</p></a>
-                        <a href="dashboard"><p className="text-white text-center m-0 font-weight-bold">Teststation-Dashboard</p></a>
+                        <p className="text-white text-center m-0 font-weight-bold">
+                            <a href="testcenter" className="text-success">Patientenübersicht-Demo</a><br />
+                            <a href="booking" className="text-success">Buchungsprozess-Demo</a><br />
+                            <a href="cases" className="text-success">Terminübersicht-Demo</a><br />
+                            <a href="managedoctors" className="text-success">Ärzteverwaltung-Demo</a><br />
+                            <a href="dashboard" className="text-success">Teststation-Dashboard</a></p>
                     </div>
                     <div className="col align-self-center mt-4 mb-4 text-center">
                         <a href="https://wirvsvirushackathon.org/"><img src={WirVsVirus} style={{ maxHeight: "8em" }} /></a>
