5ce0bfffeb7f0771094cb9820a085ee6f9a28d7e
Sean Eulenberg <sean.eulenberg@rwth-aachen.de>
2020-03-25 08:58:21 +0100

    make image responsive

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,13 +6,13 @@ export function Footer() {
         <footer className="text-muted bg-secondary" >
             <div className="container" style={{ minHeight: "15vh" }}>
                 <div className="row" style={{ minHeight: "15vh" }}>
-                    <div className="col align-self-center mt-4 mb-4 text-center">
+                    <div className="col-sm align-self-center mt-4 mb-4 text-center">
                         <p className="text-white text-center m-0 font-weight-bold">Corona Testing Information and Planning Plattform<br />
                         Check out our project on <a href="https://www.youtube.com/watch?v=UYhM87C_xqA" className="text-success">Youtube</a><br />
                         Check out our project on <a href="https://devpost.com/software/corona-atlas-de" className="text-success">Devpost</a><br />
                         Check out our project on <a href="https://github.com/hippolyt/corona-atlas" className="text-success">Github</a></p>
                     </div>
-                    <div className="col align-self-center mt-4 mb-4 text-center">
+                    <div className="col-sm align-self-center mt-4 mb-4 text-center">
                         <p className="text-white text-center m-0 font-weight-bold">
                             <a href="testcenter" className="text-success">Patientenübersicht-Demo</a><br />
                             <a href="booking" className="text-success">Buchungsprozess-Demo</a><br />
@@ -20,8 +20,8 @@ export function Footer() {
                             <a href="managedoctors" className="text-success">Ärzteverwaltung-Demo</a><br />
                             <a href="dashboard" className="text-success">Teststation-Dashboard</a></p>
                     </div>
-                    <div className="col align-self-center mt-4 mb-4 text-center">
-                        <a href="https://wirvsvirushackathon.org/"><img src={WirVsVirus} style={{ maxHeight: "8em" }} /></a>
+                    <div className="col-sm align-self-center mt-4 mb-4 text-center">
+                        <a href="https://wirvsvirushackathon.org/"><img src={WirVsVirus} className="img-fluid" style={{maxWidth: "300px"}} /></a>
                     </div>
                 </div>
             </div>
