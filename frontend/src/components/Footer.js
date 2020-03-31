641b7c9f8145a55225630daf7fe6e11f8b46b9ef
Sean Eulenberg <sean.eulenberg@rwth-aachen.de>
2020-03-31 13:06:42 +0200

    removed static links from footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,18 +7,14 @@ export function Footer() {
             <div className="container" style={{ minHeight: "15vh" }}>
                 <div className="row" style={{ minHeight: "15vh" }}>
                     <div className="col-sm align-self-center mt-4 mb-4 text-center">
-                        <p className="text-white text-center m-0 font-weight-bold">Corona Testing Information and Planning Plattform<br />
+                        <p className="text-white text-center m-0 font-weight-bold">Corona Testing Information and Planning Plattform</p>
+                        
+                    </div>
+                    <div className="col-sm align-self-center mt-4 mb-4 text-center">
+                    <p className="text-white m-0">
                         Check out our project on <a href="https://www.youtube.com/watch?v=UYhM87C_xqA" className="text-success">Youtube</a><br />
                         Check out our project on <a href="https://devpost.com/software/corona-atlas-de" className="text-success">Devpost</a><br />
                         Check out our project on <a href="https://github.com/hippolyt/corona-atlas" className="text-success">Github</a></p>
-                    </div>
-                    <div className="col-sm align-self-center mt-4 mb-4 text-center">
-                        <p className="text-white text-center m-0 font-weight-bold">
-                            <a href="testcenter" className="text-success">Patientenübersicht-Demo</a><br />
-                            <a href="booking" className="text-success">Buchungsprozess-Demo</a><br />
-                            <a href="cases" className="text-success">Terminübersicht-Demo</a><br />
-                            <a href="managedoctors" className="text-success">Ärzteverwaltung-Demo</a><br />
-                            <a href="dashboard" className="text-success">Teststation-Dashboard</a></p>
                     </div>
                     <div className="col-sm align-self-center mt-4 mb-4 text-center">
                         <a href="https://wirvsvirushackathon.org/"><img alt="WirVsVirus Hackathon Logo" src={WirVsVirus} className="img-fluid" style={{ maxWidth: "300px" }} /></a>
