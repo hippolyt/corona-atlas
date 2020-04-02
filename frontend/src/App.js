8b41f9b5f1bc1392162ecc5ef13876139d3a62db
Sean Eulenberg <sean.eulenberg@rwth-aachen.de>
2020-04-02 22:14:01 +0200

    Knopf

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import { SimpleDashboard } from "./components/demo/SimpleDashboard"
 import { useMe } from './flows/data'
 import { LoginDialog } from './components/LoginDialog'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { createBrowserHistory } from "history";
 import { Footer } from './components/Footer'
 import { DemoRootPage } from './components/demo/DemoRootPage'
 import { DemoPage } from './components/demo/DemoPage'
@@ -42,9 +43,16 @@ function TimeWindowPage() {
 }
 
 function App() {
+  const customHistory = createBrowserHistory();
   return (
-    <Router>
+    <Router history={customHistory}>
       <Navbar className="navbar navbar-dark bg-dark" style={{ height: "5vh" }}>
+        <div className="btn white">
+        <svg className="bi bi-arrow-left mr-2" onClick={customHistory.goBack} width="2em" height="2em" viewBox="0 0 16 16" fill="white" xmlns="http://www.w3.org/2000/svg">
+          <path fillRule="evenodd" d="M5.854 4.646a.5.5 0 010 .708L3.207 8l2.647 2.646a.5.5 0 01-.708.708l-3-3a.5.5 0 010-.708l3-3a.5.5 0 01.708 0z" clipRule="evenodd"/>
+          <path fillRule="evenodd" d="M2.5 8a.5.5 0 01.5-.5h10.5a.5.5 0 010 1H3a.5.5 0 01-.5-.5z" clipRule="evenodd"/>
+        </svg>
+      </div>
         <Link to="/"><Navbar.Brand >CoTip</Navbar.Brand></Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
